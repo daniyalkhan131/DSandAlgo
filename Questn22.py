bc3c4ca2d81c26199f8a16a6ff6cfa02f5452fc9
daniyalkhan131 <daniyalkhan131@gmail.com>
2024-03-24 23:13:19 +0530

    permutation of valid pranthesis

--- Questn22.py
+++ Questn22.py
@@ -9,19 +9,36 @@
 
 # Input: n = 1
 # Output: ["()"]
-
-def para(n):
-    strr=''
-    print(strr)
-    if n<=0:
+res=[]
+n=3
+def para(l,r,s):
+    if len(s)==2*n:
+        res.append(s)
+        #print(s)
         return
+    if l<n:
+        para(l+1,r,s+"(")
+    if r<l:
+        para(l,r+1,s+')')
     
-    para(n-1)
-    strr+='('
-    #print('(',end='')
-    para(n-1)
-    strr+=')'
-    #print(')',end='')
+    
     
 
-para(3)
+para(0,0,'')
+print(res)
+
+#with iteration approach
+def para(n):
+    result = []
+    left = right = 0
+    q = [(left, right, '')]
+    while q:
+        left, right, s = q.pop()
+        if len(s) == 2 * n:
+            result.append(s)
+        if left < n:
+            q.append((left + 1, right, s + '('))
+        if right < left:
+            q.append((left, right + 1, s + ')'))
+    return result
+print(para(3))
